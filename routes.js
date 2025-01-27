4f872e87c9ed44189fe2bd637296b7ad4e09155d
Joss1989 <spanies-boy@hotmail.com>
2025-01-27 11:34:45 +0100

    update

--- routes.js
+++ routes.js
@@ -1,8 +1,7 @@
-
-import express from "express";
-import morgan from 'morgan';
-import bodyParser from "body-parser";
-import dotenv from 'dotenv';
+import express, { response } from "express";
+// import morgan from 'morgan';
+// import bodyParser from "body-parser";
+import dotenv from "dotenv";
 import Data from "./models/Data.js";
 
 dotenv.config();
@@ -15,21 +14,42 @@ router.get("/test", (request, response) => {
   response.send("Hello, World root!");
 });
 
-router.get("/data", (request, response) => {
-  response.send("Hello, World data!");
-});
+// router.get("/data", (request, response) => {
+//   response.send("Hello, World data!");
+// });
 
-router.get("/getOne", (request, response) => {
-  response.send("Hello, Get One!");
-});
+// router.get("/getOne", (request, response) => {
+//   response.send("Hello, Get One!");
+// });
 
 router.get("/getAlldata", async (request, response) => {
-
   try {
     const cards = await Data.find();
     response.json(cards);
   } catch (error) {
     response.status(500).json({ message: error.message });
+  }
+});
+
+router.post("/post", async (req, res) => {
+
+  const formData = req.body;
+
+  const data = new Data({
+    name: formData.name,
+    img: formData.img,
+    information: { 
+      strength: formData.strength, 
+      lives: formData.lives 
+    },
+    text: formData.text
+  })
+
+  try {
+    const dataToSave = await data.save()
+    res.status(200).json(dataToSave)
+  } catch (error) {
+    res.status(400).json({ message: error.message })
   }
 });
 
